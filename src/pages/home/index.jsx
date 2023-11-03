2543408783654d9779f635b80fdbe41b16aeca12
ViniciusSLima2102 <vinicius.lima@opikode.com>
2023-11-02 23:03:56 -0300

    add brands

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,7 @@
+import BrandCard from "@/ui/components/BrandCard";
 import BenefitsInfo from "@/ui/partials/BenefitsInfo";
 import Header from "@/ui/partials/Header";
+import Brands from "@/ui/partials/Brands";
 import HomeBanner from "@/ui/partials/HomeBanner";
 
 const Home = () => {
@@ -8,6 +10,14 @@ const Home = () => {
             <Header></Header>
             <HomeBanner></HomeBanner>
             <BenefitsInfo></BenefitsInfo>
+            
+            <Brands>
+                <BrandCard srcImg="nike-logo.png" brandName="Nike" hrefLink="/products/nike"></BrandCard>
+                <BrandCard srcImg="adidas-logo.webp" brandName="Adidas" hrefLink="/products/adidas"></BrandCard>
+                <BrandCard srcImg="puma-logo.png" brandName="Puma" hrefLink="/products/puma"></BrandCard>
+                <BrandCard srcImg="reserva-logo.jpg" brandName="Reserva" fontColor="red" hrefLink="/products/reserva"></BrandCard>
+                <BrandCard srcImg="olympikus-logo.png" brandName="Olympikus" hrefLink="/products/olympikus"></BrandCard>
+            </Brands>
         </div>
         
     )
---
+++ src/ui/components/BrandCard/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./index.module.css"
+import Link from "next/link";
+const BrandCard = (props) => {
+    return (
+        <Link className={styles["card-container"]} href={props.hrefLink}>
+            <img src={props.srcImg} alt={props.imgAlt} />
+            <p className={props.fontColor ? styles[props.fontColor] : styles["basic"]}>{props.brandName}</p>
+        </Link>
+    )
+}
+
+export default BrandCard;
